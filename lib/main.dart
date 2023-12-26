a7a1ec1a04dc4356767fb84d6191db4da5f94466
oluwasniper <132602272+oluwasniper@users.noreply.github.com>
2023-12-26 17:47:09 +0100

    added container

--- lib/main.dart
+++ lib/main.dart
@@ -18,22 +18,44 @@ class MyApp extends StatelessWidget {
           children: [
             CircleAvatar(
               radius: 50.0,
-              backgroundImage: AssetImage(''),
+              backgroundImage: AssetImage('assets/mure.jpg'),
             ),
             Text('Ogundipe Emmanuel Oluwamurewa'),
             Text('Flutter Developer'),
-            Container(
+            Card(
+              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
               color: Colors.white,
-              child: Row(
-                children: [
+              child: Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Row(
+                  children: [
+                    Icon(
+                      Icons.phone,
+                      color: Colors.black,
+                    ),
+                    SizedBox(
+                      width: 10.0,
+                    ),
+                    Text(
+                      '+234 708 335 0453',
+                      style: TextStyle(color: Colors.black),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Card(
+              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+              color: Colors.white,
+              child: Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Row(children: [
+                  Icon(Icons.email, color: Colors.black),
                   SizedBox(
                     width: 10.0,
                   ),
-                  Icon(
-                    Icons.phone,
-                    color: Colors.black,
-                  ),
-                ],
+                  Text('demiladeogundipe@gmail.com'),
+                ]),
               ),
             )
           ],

7d6746f063ac73489fd734e83c4dd2b861a38769
oluwasniper <132602272+oluwasniper@users.noreply.github.com>
2024-01-02 16:10:19 +0100

    floating action button added

--- lib/main.dart
+++ lib/main.dart
@@ -13,85 +13,89 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        drawer: Drawer(
-          child: ListView(
-            children: [
-              const DrawerHeader(
-                decoration: BoxDecoration(
-                  color: Colors.white,
+          drawer: Drawer(
+            child: ListView(
+              children: [
+                const DrawerHeader(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                  ),
+                  child: Text('Drawer Header'),
                 ),
-                child: Text('Drawer Header'),
-              ),
-              ListTile(
-                title: const Text('For Bookings'),
-                onTap: () {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => const BookingsPage()));
-                },
-              ),
-              ListTile(
-                title: const Text('Item 2'),
-                onTap: () {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => const BookingsPage()));
-                },
-              ),
-            ],
-          ),
-        ),
-        backgroundColor: Colors.teal,
-        body: const SafeArea(
-            child: Column(
-          children: [
-            CircleAvatar(
-              radius: 50.0,
-              backgroundImage: AssetImage('assets/mure.jpg'),
+                ListTile(
+                  title: const Text('For Bookings'),
+                  onTap: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const BookingsPage()));
+                  },
+                ),
+                ListTile(
+                  title: const Text('Item 2'),
+                  onTap: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const BookingsPage()));
+                  },
+                ),
+              ],
             ),
-            Text('Ogundipe Emmanuel Oluwamurewa'),
-            Text('Flutter Developer'),
-            Card(
-              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-              color: Colors.white,
-              child: Padding(
-                padding: EdgeInsets.all(20.0),
-                child: Row(
-                  children: [
-                    Icon(
-                      Icons.phone,
-                      color: Colors.black,
-                    ),
+          ),
+          backgroundColor: Colors.teal,
+          body: const SafeArea(
+              child: Column(
+            children: [
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('assets/mure.jpg'),
+              ),
+              Text('Ogundipe Emmanuel Oluwamurewa'),
+              Text('Flutter Developer'),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                color: Colors.white,
+                child: Padding(
+                  padding: EdgeInsets.all(20.0),
+                  child: Row(
+                    children: [
+                      Icon(
+                        Icons.phone,
+                        color: Colors.black,
+                      ),
+                      SizedBox(
+                        width: 10.0,
+                      ),
+                      Text(
+                        '+234 708 335 0453',
+                        style: TextStyle(color: Colors.black),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                color: Colors.white,
+                child: Padding(
+                  padding: EdgeInsets.all(20.0),
+                  child: Row(children: [
+                    Icon(Icons.email, color: Colors.black),
                     SizedBox(
                       width: 10.0,
                     ),
-                    Text(
-                      '+234 708 335 0453',
-                      style: TextStyle(color: Colors.black),
-                    ),
-                  ],
+                    Text('demiladeogundipe@gmail.com'),
+                  ]),
                 ),
-              ),
-            ),
-            Card(
-              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-              color: Colors.white,
-              child: Padding(
-                padding: EdgeInsets.all(20.0),
-                child: Row(children: [
-                  Icon(Icons.email, color: Colors.black),
-                  SizedBox(
-                    width: 10.0,
-                  ),
-                  Text('demiladeogundipe@gmail.com'),
-                ]),
-              ),
-            )
-          ],
-        )),
-      ),
+              )
+            ],
+          )),
+          floatingActionButton: FloatingActionButton(
+            onPressed: () {},
+            child: Icon(Icons.add),
+            backgroundColor: Colors.black,
+          )),
     );
   }
 }
